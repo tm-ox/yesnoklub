310c0dca7ccb7aebdc60ee6d10bfd82c71227d92
tm-ox <timothy@oxenfree.net>
2022-07-27 10:21:26 +0700

    Metatags

--- components/Head.vue
+++ components/Head.vue
@@ -2,27 +2,57 @@
   <Head>
     <!-- Primary Meta Tags -->
     <Title>Yes No Klub - Experimental etc.</Title>
-    <Meta name="title" content="Yes No Klub - Experimental etc. event organiser out of Yogyakarta, Indonesia.">
-    <Meta name="description" content="Yes No Klub utilises multiple venues and formats to facilitate local & international artists with touring and collaborative projects throughout southeast asia and beyond.">
+    <Meta
+      name="title"
+      content="Yes No Klub - Experimental etc. event organiser out of Yogyakarta, Indonesia."
+    />
+    <Meta
+      name="description"
+      content="Yes No Klub utilises multiple venues and formats to facilitate local & international artists with touring and collaborative projects throughout southeast asia and beyond."
+    />
 
     <!-- Open Graph / Facebook -->
-    <Meta property="og:type" content="website">
-    <Meta property="og:url" content="https://metatags.io/">
-    <Meta property="og:title" content="Yes No Klub - Experimental etc. event organiser out of Yogyakarta, Indonesia.">
-    <Meta property="og:description" content="Yes No Klub utilises multiple venues and formats to facilitate local & international artists with touring and collaborative projects throughout southeast asia and beyond.">
-    <Meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+    <Meta property="og:type" content="website" />
+    <Meta property="og:url" content="https://metatags.io/" />
+    <Meta
+      property="og:title"
+      content="Yes No Klub - Experimental etc. event organiser out of Yogyakarta, Indonesia."
+    />
+    <Meta
+      property="og:description"
+      content="Yes No Klub utilises multiple venues and formats to facilitate local & international artists with touring and collaborative projects throughout southeast asia and beyond."
+    />
+    <Meta
+      property="og:image"
+      content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+    />
 
     <!-- Twitter -->
-    <Meta property="twitter:card" content="summary_large_image">
-    <Meta property="twitter:url" content="https://metatags.io/">
-    <Meta property="twitter:title" content="Yes No Klub - Experimental etc. event organiser out of Yogyakarta, Indonesia.">
-    <Meta property="twitter:description" content="Yes No Klub utilises multiple venues and formats to facilitate local & international artists with touring and collaborative projects throughout southeast asia and beyond.">
-    <Meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+    <Meta property="twitter:card" content="summary_large_image" />
+    <Meta property="twitter:url" content="https://metatags.io/" />
+    <Meta
+      property="twitter:title"
+      content="Yes No Klub - Experimental etc. event organiser out of Yogyakarta, Indonesia."
+    />
+    <Meta
+      property="twitter:description"
+      content="Yes No Klub utilises multiple venues and formats to facilitate local & international artists with touring and collaborative projects throughout southeast asia and beyond."
+    />
+    <Meta
+      property="twitter:image"
+      content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+    />
 
-    <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
-    <Script defer src="https://unpkg.com/@alpinejs/intersect@3.x.x/dist/cdn.min.js"></Script>
-    <Script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></Script>
+    <Script
+      src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+    ></Script>
+    <Script
+      defer
+      src="https://unpkg.com/@alpinejs/intersect@3.x.x/dist/cdn.min.js"
+    ></Script>
+    <Script
+      defer
+      src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"
+    ></Script>
   </Head>
 </template>
-
-
